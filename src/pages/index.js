150fbcea1de36b903f6eb0803e21e8816cb3e8c0
Adam <a.glebicki@gmail.com>
2018-12-05 16:21:40 +0100

    Add styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,74 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
+import Card from '../components/Card'
+import Section from '../components/Section'
+import Wave from '../components/Wave'
 
 const IndexPage = () => (
   <Layout>
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>Learn to design and code React apps</h1>
+        <h1>Learn to <br /> design and code React apps</h1>
         <p>Complete courses about best tools and design systems. Proptotype and build apps with React and Figma.</p>
         <Link to="/page-2/">Watch the video</Link>
+        <div className="Logos">
+          <img src={require('../images/logo-sketch.png')} width="50" />
+          <img src={require('../images/logo-figma.png')} width="50" />
+          <img src={require('../images/logo-studio.png')} width="50" />
+          <img src={require('../images/logo-framer.png')} width="50" />
+          <img src={require('../images/logo-react.png')} width="50" />
+          <img src={require('../images/logo-swift.png')} width="50" />
+        </div>
+        <Wave />
       </div>
+      <div className="Cards">
+        <h2>11 courses, more comming</h2>
+        <div className="CardGroup">
+          <Card 
+            title='React for Designers' 
+            text='12 sections'
+            image={require('../images/wallpaper2.jpg')}
+          />
+          <Card 
+            title='Design Systems' 
+            text='10 sections'
+            image={require('../images/wallpaper.jpg')}
+          />
+          <Card 
+            title='Sound Design' 
+            text='5 sections'
+            image={require('../images/wallpaper4.jpg')}
+          />
+          <Card 
+            title='ARKit 2' 
+            text='10 sections'
+            image={require('../images/wallpaper4.jpg')}
+          />
+          <Card 
+            title='Figma for Designers' 
+            text='11 sections'
+            image={require('../images/wallpaper2.jpg')}
+          />
+          <Card 
+            title='Modern CSS' 
+            text='14 sections'
+            image={require('../images/wallpaper.jpg')}
+          />
+        </div>
+      </div>
+      <Section 
+        image={require('../images/wallpaper2.jpg')}
+        logo={require('../images/logo-react.png')}
+        title="React for Designers"
+        text='Learn how to buid a modern site using React and most efficient libraries to get your site/product online. 
+        Get familiar with components, CSS Grid, animations, interactions, 
+        dynamic data with Contentful and deploying your site with Netlify.'
+      />
     </div>
   </Layout>
 )
 
 export default IndexPage
+
+
